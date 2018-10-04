217ccc2f0fe1cedb909eb3bc6b66a733567d9c87
ohjihwan-nbt <oh.jihwan@nbt.com>
2018-10-04 16:49:57 +0900

    수정: nuxt.config.js의 head title 변경
    - 잘못 작성된 이름 수정

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'smallbites-web',
+    title: 'default-nuxt-structure',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
