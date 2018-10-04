b4c09507083d8c8ac8bab2f7dee8129b7a491e3a
ohjihwan-nbt <oh.jihwan@nbt.com>
2018-10-04 17:40:08 +0900

    수정: index 페이지 Swiper 컴포넌트 사용하도록 변경
    - index가 생성된 시점에 swiper에게 isCreated prop을 전달
    - Swiper는 isCreated에 대해 watch를 수행하다 변경을 감지하면 다음 틱에 이미지 너비만큼의 최대높이를 가지도록함
    - 다른 page에서 Swiper 컴포넌트 사용 시 현재의 구조대로 사용할 것

--- pages/index.vue
+++ pages/index.vue
@@ -4,14 +4,7 @@
     <section class="body" v-show="isCreated">
       <div>{{title}}</div>
 
-      <div v-swiper:mySwiper="swiperOption" ref="swiperBox">
-        <div class="swiper-wrapper">
-          <div class="swiper-slide" v-for="banner in banners" :key="banner">
-            <img :src="banner">
-          </div>
-        </div>
-        <div class="swiper-pagination swiper-pagination-bullets"></div>
-      </div>
+      <swiper :isCreated="isCreated"></swiper>
     </section>
   </section>
 </template>
@@ -19,48 +12,18 @@
 <script>
 import axios from 'axios'
 import Loading from '~/components/Loading.vue'
+import Swiper from '~/components/Swiper.vue'
 
 export default {
-  components: { Loading },
+  components: { Loading, Swiper },
   data() {
     return {
       isCreated: false,
       title: 'Hello World!',
-      // 모든 이미지는 정사각형이여야한다.
-      banners: [
-        'https://via.placeholder.com/1000x1000',
-        'https://via.placeholder.com/500x500',
-        'https://via.placeholder.com/550x550',
-        'https://via.placeholder.com/880x880',
-        'https://via.placeholder.com/450x450'
-      ],
-      swiperOption: {
-        loop: true,
-        slidesPerView: 'auto',
-        centeredSlides: true,
-        spaceBetween: 0,
-        pagination: {
-          el: '.swiper-pagination',
-          dynamicBullets: true
-        },
-        on: {
-          slideChange() {
-            console.log('onSlideChangeEnd', this);
-          },
-          tap() {
-            console.log('onTap', this);
-          }
-        }
-      }
     }
   },
   mounted() {
     this.isCreated = true
-    this.$nextTick(_=>{
-      const swiperBox = this.$refs.swiperBox
-      const swiperBoxStyles = window.getComputedStyle(swiperBox)
-      swiperBox.style.maxHeight = swiperBoxStyles.width
-    })
   },
 }
 </script>
