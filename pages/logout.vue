784eeffe1a2cc32a3dcb223f22185ccc912658d7
ohjihwan-nbt <oh.jihwan@nbt.com>
2018-10-05 19:49:06 +0900

    추가: logout 페이지 작성

---
+++ pages/logout.vue
@@ -0,0 +1,15 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  async mounted() {
+    this.$store.commit('logout')
+    // 스토어로부터 로그인 상태 확인 후 로그인 상태인 경우
+    // 토큰 폐기처리 등 작업 처리
+    // 모든 비동기 처리가 완료된 경우 메인으로 이동
+    location.href = '/'
+  }
+}
+</script>
