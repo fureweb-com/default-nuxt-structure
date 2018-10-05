513f8a70240ac33b97da95204ce7f3d08a89d62d
ohjihwan-nbt <oh.jihwan@nbt.com>
2018-10-05 14:40:25 +0900

    추가: node.js 환경에서 사용할 상수 객체 추가

---
+++ constants.js
@@ -0,0 +1,12 @@
+module.exports = {
+  ENVIRONMENT: {
+    DEVELOPMENT: 'development',
+    STAGE      : 'stage',
+    PRODUCTION : 'production',
+  },
+  API_SERVER_HOST: {
+    'development': '0.0.0.0',
+    'stage'      : '0.0.0.0',
+    'production' : '0.0.0.0'
+  }
+}
